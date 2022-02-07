2def0f7328bb5392e20efa228a1b710f19f472b9
Airit Simsel <airitkarm@gmail.com>
2022-02-07 15:13:51 +0200

    first version

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require ('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const aggregation = require('aggregation');
 
-mongoose.connect('mongodb://localhost:27017/galleryDB', {useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/wishListDB', {useUnifiedTopology: true});
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -12,7 +13,8 @@ app.use(express.static('images'));
 
 imageSchema = new mongoose.Schema ({
     imageDescription: String,
-    image: String
+    image: String,
+    wish: String
 });
 
 imageModel = mongoose.model('Image', imageSchema)    // create image out of the imageSchema, andmebaasi tekib automaatselt images
@@ -29,8 +31,8 @@ let upload = new multer({
     })
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/admin', (req, res) => {
+    res.render('admin');
 });
 
 app.post('/upload', upload.single('userFile'), (req, res) => {     //inputi kasti nimi name="userFile"
@@ -39,27 +41,39 @@ app.post('/upload', upload.single('userFile'), (req, res) => {     //inputi kast
     let newImage = new imageModel();
     newImage.imageDescription = req.body.description;
     newImage.image = req.file.filename;
+    newImage.wish = req.body.wish;
 
     newImage.save((error, document) => {
         if(!error) {
             console.log('file saved');
-            res.redirect('gallery');
+            res.redirect('/');
         } else {
             console.log(error);
         }
     });
 });   
 
-app.get('/gallery', (req, res) => {
+app.get('/', (req, res) => {
     imageModel.find()
     .then(document => {
         console.log(document);
-        res.render('gallery', {item: document});
+        res.render('index', {item: document});
     });
 });
- 
 
-
+app.get('/random', (req, res) => {
+    imageModel.find().then((document) => {
+  
+      const randomWish = Math.floor(Math.random() * document.length);
+      let random_wish = document[randomWish];
+  
+      console.log(random_wish);
+  
+      res.render('random', { item: random_wish });    
+    });
+  });
+                    
+      
 const port = 5000;
 
 app. listen(port, () => {
